20c3729db5ec4348c7165bd3feae55c93db9ed45
keesdewit82 <keesdewit@keesdewit.nl>
2017-04-06 10:03:19 +0200

    Rename fix

--- share/qt/make_windows_icon.sh
+++ share/qt/make_windows_icon.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # create multiresolution windows icon
-ICON_DST=../../src/qt/res/icons/novacoin.ico
+ICON_DST=../../src/qt/res/icons/amsterdamcoin.ico
 
 convert ../../src/qt/res/icons/amsterdamcoin-16.png ../../src/qt/res/icons/amsterdamcoin-32.png ../../src/qt/res/icons/amsterdamcoin-48.png ${ICON_DST}
